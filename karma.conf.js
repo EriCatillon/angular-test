2ea41c704e2516945a89c4b68828bd8cd43b0d29
EriCatillon <ericatillon@gmail.com>
2015-03-24 14:59:06 +0100

    yml'

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+        'bower_components/angular/angular.js',
     ],
 
 
@@ -59,6 +60,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
